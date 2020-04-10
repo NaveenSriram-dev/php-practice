33135e65ca9a61cf6446ec3f4005d3d760236fb0
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2020-04-10 13:09:31 +0530

    Initial commit

---
+++ basics.php
@@ -0,0 +1,6 @@
+<?php
+  $name ="Naveen Sriram";
+  $chumma ="My name is Naveen Sriram";
+//  echo $chumma.$name;
+  echo str_replace($name,"Billa",$chumma); //this is a return type func
+?>
---
+++ str_manipulation.php
@@ -0,0 +1,9 @@
+<?php
+  $name = "Naveen Sriram";
+  $string ="my name is something";
+  echo ucwords($string);
+  echo "<br>";
+  echo substr($string,11,4)."<br>";
+  echo strtoupper($name)."<br>";
+  echo strtolower($name);
+?>
